ee64d3c58c1615670289a218c060bf4a7511edc9
Aniket Kumar <kumaraniket6219@gmail.com>
2025-10-07 15:46:47 +0530

    Update ODIRatings.spec.js

--- tests/ODIRatings.spec.js
+++ tests/ODIRatings.spec.js
@@ -1,5 +1,5 @@
 import {test} from '@playwright/test'
-test('test1',async ({page})=>{
+test('test1',{tag:'@smoke'},async ({page})=>{
     await page.goto('https://www.cricbuzz.com/cricket-stats/icc-rankings/men/batting')
     await page.locator('#batsmen-odis-tab').click()
     let ratings=await page.locator('//div[contains(@ng-show,"batsmen-odis")]/descendant::div[contains(@class,"cb-rank-tbl pull-right")][number(translate(text()," ",""))>700]').all()
